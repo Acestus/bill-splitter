9e560223062d570bb29c4ac69fd5ce2c45e683fd
acestus <architect@acestus.com>
2022-11-10 09:56:54 -0600

    Created Random Module

--- Topics/For loop/Vowel count/main.py
+++ Topics/For loop/Vowel count/main.py
@@ -1,3 +1,5 @@
+import TODO as TODO
+
 string = "red yellow fox bite orange goose beeeeeeeeeeep"
 vowels = 'aeiou'
 counter = 0
@@ -6,4 +8,5 @@ for i in vowels:
     #print(i, string.count(i))
     counter += string.count(i)
 print(counter)
+
 
